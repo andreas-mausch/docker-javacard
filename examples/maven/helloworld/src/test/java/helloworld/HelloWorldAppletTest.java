c4a603bd3d4d0d39781338e718d460897ecfc4db
neonew <neonew@gmail.com>
2025-10-06 21:16:24 +0200

    Refactor tests to easily support multiple test cases

--- examples/maven/helloworld/src/test/java/helloworld/HelloWorldAppletTest.java
+++ examples/maven/helloworld/src/test/java/helloworld/HelloWorldAppletTest.java
@@ -5,10 +5,13 @@ import static org.assertj.core.api.Assertions.*;
 import helloworld.HelloWorldApplet;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import javax.smartcardio.CommandAPDU;
 import javax.smartcardio.ResponseAPDU;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.licel.jcardsim.bouncycastle.util.encoders.Hex;
@@ -19,14 +22,14 @@ import javacard.framework.AID;
 
 public class HelloWorldAppletTest {
 
+    private static final String PACKAGE_AID_HEX = "01020304050607";
     private static CardSimulator simulator;
 
-    @Test
-    public void testPing() throws Exception {
-        CardSimulator simulator = new CardSimulator();
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        simulator = new CardSimulator();
 
         // Install applet
-        String PACKAGE_AID_HEX = "01020304050607";
         byte[] aid_bytes = Hex.decode(PACKAGE_AID_HEX + "01");
 
         AID aid = AIDUtil.create(aid_bytes);
@@ -36,8 +39,19 @@ public class HelloWorldAppletTest {
 
         simulator.installApplet(aid, HelloWorldApplet.class, bos.toByteArray(), (short) 0, (byte) bos.size());
         bos.close();
+    }
 
+    @BeforeEach
+    void beforeEach() throws IOException {
+        simulator.reset();
+
+        byte[] aid_bytes = Hex.decode(PACKAGE_AID_HEX + "01");
+        AID aid = AIDUtil.create(aid_bytes);
         simulator.selectApplet(aid);
+    }
+
+    @Test
+    public void testPing() {
         CommandAPDU apdu = new CommandAPDU(0x80, 0x00, 0, 0);
         ResponseAPDU result = simulator.transmitCommand(apdu);
         assertThat(result.getSW()).isEqualTo(0x9000);
